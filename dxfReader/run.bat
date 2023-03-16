74a2e8ab3b412194c822ad75ee8783348e5bfe61
ltsai323 <johnson20050@gmail.com>
2023-03-16 15:14:25 +0800

    batch file accomplished

--- dxfReader/run.bat
+++ dxfReader/run.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-ECHO "%~1"
-ECHO python3.exe dxfReader.py "%~1"
-ECHO python3.exe sortedPoint.py output_dxfReader.txt
+ECHO input file is : "%~1"
+python3.exe dxfReader.py "%~1" && python3.exe sortedPoint.py output_dxfReader.txt
+pause
