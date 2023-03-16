f3dedc01dc93767c05cd8157c538203326f41c3e
ltsai323 <johnson20050@gmail.com>
2023-03-16 15:57:30 +0800

    accomplish run.bat. It is not a PowerShell. It is CMD.exe

--- dxfReader/run.bat
+++ dxfReader/run.bat
@@ -1,4 +1,12 @@
 @ECHO OFF
-ECHO input file is : "%~1"
-python3.exe dxfReader.py "%~1" && python3.exe sortedPoint.py output_dxfReader.txt
+
+SET "inputfile=%~1"
+if "%inputfile%" == "" (
+   ECHO "You need to drag a DXF file for execute further command"
+   pause
+   exit
+)
+
+ECHO input file is : "%inputfile%"
+python3.exe dxfReader.py "%inputfile%" && python3.exe sortedPoint.py output_dxfReader.txt
 pause
