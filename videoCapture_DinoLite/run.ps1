d5a1917d1c4c8da334bf21b3083ed3c3b2fc7461
ltsai323 <johnson20050@gmail.com>
2024-07-22 14:36:44 +0800

    sync

--- videoCapture_DinoLite/run.ps1
+++
@@ -1,5 +0,0 @@
-.\scripts\env_setup.ps1
-.\scripts\dependencies.ps1
-python3 .\main.py
-
-Read-Host -Prompt "Press any key to continue..."
--- videoCapture_DinoLite/scripts/run.ps1
+++ videoCapture_DinoLite/scripts/run.ps1
@@ -1,5 +1,4 @@
 .\scripts\env_setup.ps1
-#.\scripts\dependencies.ps1
 python3 .\main.py
 
-Read-Host -Prompt "Press any key to continue..."
+Read-Host -Prompt "Press Enter to continue..."
