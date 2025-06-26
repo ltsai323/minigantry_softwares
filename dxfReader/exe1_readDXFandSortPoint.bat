1a0369448b57b8fe35cebc6503acc68c52f75e21
ltsai323 <johnson20050@gmail.com>
2025-06-26 16:51:51 +0800

    update batch file

--- dxfReader/exe1_readDXFandSortPoint.bat
+++ dxfReader/exe1_readDXFandSortPoint.bat
@@ -1,12 +1,20 @@
 @ECHO OFF
 
-SET "inputfile=%~1"
-if "%inputfile%" == "" (
+SET "filename=%~1"
+if "%filename%" == "" (
    ECHO "You need to drag a DXF file for execute further command"
    pause
    exit
 )
+:: Call PowerShell to show OpenFileDialog if filename not dragged
+if "%filename%" == "" (
+  set "script_dir=%~dp0"
 
-ECHO input file is : "%inputfile%"
-python3.exe .\python\dxfReader.py "%inputfile%" && python3.exe .\python\sortedPoint.py step1_dxfReader.txt 0
+  for /f "usebackq delims=" %%f in (`powershell -nologo -command "Add-Type -AssemblyName System.Windows.Forms; $ofd = New-Object System.Windows.Forms.OpenFileDialog; $ofd.InitialDirectory = '%script_dir%'; $ofd.Title = 'Select a hexaboard DXF file'; $ofd.Filter = 'DXF Files (*.dxf)|*.dxf|All files (*.*)|*.*'; if ($ofd.ShowDialog() -eq 'OK') { $ofd.FileName }"`) do (
+    set "filename=%%f"
+  )
+)
+
+ECHO input file is : "%filename%"
+python3.exe .\python\dxfReader.py "%filename%" && python3.exe .\python\sortedPoint.py step1_dxfReader.txt 0
 pause
